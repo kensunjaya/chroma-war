58f68eecb8ae910a8bd26af3065ae5a268cd1891
kensunjaya <kennethsunjaya@gmail.com>
2025-06-13 21:01:02 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,63 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
+
+type Cell = {
+  val: number;
+  color: Color;
+}
+
+type Color = "red-400" | "blue-400" | "white";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [cells, setCells] = useState<Cell[][]>(Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => ({ val: 0, wrong: false, color: "white" }))));
+  const [turn, setTurn] = useState<number>(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleClick = (row: number, col: number, turn: number) => {
+    let color: Color = turn % 2 === 0 ? "blue-400" : "red-400";
+    if (cells[row][col].val === 0) {
+      if (turn > 1) {
+        return;
+      }
+    }
+    if (cells[row][col].color !== "white" && color !== cells[row][col].color) {
+      return;
+    }
+
+    setTurn(turn + 1);
+    color = turn % 2 === 0 ? "blue-400" : "red-400";
+    recursiveFill(row, col, color);
+  }
+  const recursiveFill = (row: number, col: number, color: Color) => {
+    cells[row][col].val += 1;
+    cells[row][col].color = color;
+    setCells([...cells]);
+    if (cells[row][col].val === 4) {
+      cells[row][col].val = 0;
+      cells[row][col].color = "white";
+      setCells([...cells]);
+      if (row > 0) recursiveFill(row - 1, col, color); // Up
+      if (row < 8) recursiveFill(row + 1, col, color); // Down
+      if (col > 0) recursiveFill(row, col - 1, color); // Left
+      if (col < 8) recursiveFill(row, col + 1, color); // Right
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center min-h-screen py-3 sm:py-4 font-sans">
+      <div className="grid mt-4 sm:mt-5 grid-cols-9 text-3xl xs:text-3xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-5xl">
+        {cells.map((row, rowIndex) => row.map((cell, colIndex) =>
+          <div
+            onClick={() => { handleClick(rowIndex, colIndex, turn) }}
+            // onKeyDown={(event) => handleKeyDown(event, rowIndex, colIndex)}
+            tabIndex={0}
+            key={rowIndex * 9 + colIndex}
+            className={`flex border text-white dark:text-black bg-${cell.color} rounded-xl justify-center hover:cursor-pointer items-center h-[2.6rem] w-[2.6rem] xs:h-14 xs:w-14 sm:h-14 sm:w-14 md:h-16 md:w-16 xl:h-18 xl:w-18 m-2 select-none`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            {cell.val === 0 ? "" : cell.val}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
