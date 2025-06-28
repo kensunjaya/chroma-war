5b3433d1fdf46168b844b7769ebdc7b1e13ac5ce
Kenneth sunjaya <kennethsunjaya@gmail.com>
2025-06-28 12:55:08 +0700

    update socket connection to use only websocket and polling transports

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,7 +2,7 @@
 import { io, Socket } from 'socket.io-client';
 
 const socket: Socket = io(process.env.NEXT_PUBLIC_GAME_SERVER as string, {
-  autoConnect: false,
+  transports: ['websocket', 'polling'],
 });
 
 export default socket;
